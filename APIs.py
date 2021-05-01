5bc9fd3154712e2f17073b7fa881d2684c7426da
Dillon <43819022+Dcode99@users.noreply.github.com>
2021-05-01 17:13:49 -0400

    Update APIs.py

--- APIs.py
+++ APIs.py
@@ -1,42 +1,64 @@
 import json
 import requests
 import pymongo
+import DBtools
+import pymysql
 
-# Getting mongoDB setup
-# TO DO: Add database URL
-  client = pymongo.MongoClient(database_url)
-  
-  # Connecting to the database
-  mydb = client[patients]
 
-# APIs
+def getAPI():
+    resp = requests.get('http://deta224.cs.uky.edu:9000')
+    return resp.json()
+
+
+def mongo_connect():
+    # Getting mongoDB setup
+    # TO DO: Add database URL
+    database_url = '127.0.0.1'
+    client = pymongo.MongoClient(database_url)
+
+    # Connecting to the database
+    mydb = client['patients']
+
+    # Connecting the to collection
+    # TO DO: add collection name
+    col = mydb[collection_name]
+    d = col.delete_many()
+
+
+# restart mysql database and set cursor
+dbCursor = DBtools.restartDB()
+# get API
+path = getAPI()
+
 # getteam API
 if path is "/getteam":
-  status = 1
-  team_info = {
-    'team_name': 'Arnoldillo', 
-    'Team_member_sids': {912141777, 900000000},
-    'app_status_code' : status
-  }
-  jsonString = json.dump(team_info)
+    status = 1
+    team_info = {
+        'team_name': 'Arnoldillo',
+        'Team_member_sids': {912141777, 900000000},
+        'app_status_code': status
+    }
+    jsonString = json.dumps(team_info)
 
-# reset API
-else if path is "/reset":
-  
-  # Connecting the to collection
-  # TO DO: add collection name
-  col = mydb[collection_name]
-  d = col.delete_many()
-  return_info = {
-    'reset_status_code' : '1'
-  }
-  jsonString = json.dump(return_info)
-  
-  else if path is "/gethospital":
-    query = "SELECT max_capacity AS total_beds, (max_capacity-current_capacity)"
-    + " AS available_beds,"
-    + " zip_code FROM hospitals"
-    + " WHERE hospital_id is " + id
-    
-    return_info = mycol.find(myquery)
-    jsonString = json.dump(return_info)
+# reset API, needs to reset relational DB and CEP
+elif path is "/reset":
+    try:
+        # Connecting the to collection
+        dbCursor = DBtools.restartDB()
+        return_info = {
+            'reset_status_code': 1
+        }
+    except Exception as e:
+        return_info = {
+            'reset_status_code': 0
+        }
+    jsonString = json.dumps(return_info)
+
+elif path is "/gethospital":
+    query = "SELECT max_capacity AS total_beds," \
+            " (max_capacity-current_capacity) AS available_beds," \
+            " zip_code FROM hospitals" \
+            " WHERE hospital_id is " + str(id)
+
+    return_info = dbCursor.execute(query)
+    jsonString = json.dumps(return_info)
