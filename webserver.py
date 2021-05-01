b566a816cc51ac6ea90bb5ece73f1f40c7f209d5
Dillon <43819022+Dcode99@users.noreply.github.com>
2021-05-01 17:14:14 -0400

    Moved API

--- webserver.py
+++ webserver.py
@@ -8,8 +8,3 @@ PORT = 9000
 with socketserver.TCPServer(("", PORT), Handler) as http:
     print("serving at port", PORT)
     http.serve_forever()
-
-
-def getAPI():
-    resp = requests.get('http://deta224.cs.uky.edu:9000')
-    return resp.json()
