590881065f8f4b507a4631fe1f64850a6aaaf41e
Dillon <43819022+Dcode99@users.noreply.github.com>
2021-05-01 19:49:53 -0400

    Update Subscriber.py

--- Subscriber.py
+++ Subscriber.py
@@ -17,9 +17,9 @@ virtualhost = '13'
 
 credentials = pika.PlainCredentials(username, password)
 parameters = pika.ConnectionParameters(hostname,
-                                           5672,
-                                           virtualhost,
-                                           credentials)
+                                       5672,
+                                       virtualhost,
+                                       credentials)
 
 connection = pika.BlockingConnection(parameters)
 
@@ -49,29 +49,31 @@ def callback(ch, method, properties, body):
     # editing to get to utf-8
     bodystr = body.decode('utf-8')
     data = json.loads(bodystr)
-    
+
     # split payloads
     for payload in data:
-      fname = payload['first_name']
-      lname = payload['last_name']
-      mrn = payload['mrn']
-      zipcode = payload['zipcode']
-      patient_status = payload['patient_status_code']
-      
+        fname = payload['first_name']
+        lname = payload['last_name']
+        mrn = payload['mrn']
+        zipcode = payload['zipcode']
+        patient_status = payload['patient_status_code']
+
     # count positive and negative cases
     a, b = DBtools.case_count(patient_status)
-    positive count += a
+    positive_count += a
     negative_count += b
-      
+
     # find closest open hospital, if needed
     # CODE NEEDED
-    
-    # 
+
+    #
+
 
 channel.basic_consume(
     queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 channel.start_consuming()
+
 
 def case_count(status):
     # negative test + 1
@@ -83,7 +85,8 @@ def case_count(status):
     # else not tested
     else:
         return 0, 0
-  
- def reset_count():
+
+
+def reset_count():
     positive_count = 0
     negative_count = 0
