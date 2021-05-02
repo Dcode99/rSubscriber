73b8d58948f3b0e3476635611e41fcc1faa8fb00
Dillon <43819022+Dcode99@users.noreply.github.com>
2021-05-02 13:59:11 -0400

    Added main guard

--- Subscriber.py
+++ Subscriber.py
@@ -27,41 +27,42 @@ def reset_count():
     negative_count = 0
 
 
-# Set the connection parameters to connect to rabbit-server1 on port 5672
-# on the / virtual host using the username "guest" and password "guest"
+if __name__ == "__main__":
+    # Set the connection parameters to connect to rabbit-server1 on port 5672
+    # on the / virtual host using the username "guest" and password "guest"
 
-username = 'student'
-password = 'student01'
-hostname = '128.163.202.50'
-virtualhost = '13'
+    username = 'student'
+    password = 'student01'
+    hostname = '128.163.202.50'
+    virtualhost = '13'
 
-credentials = pika.PlainCredentials(username, password)
-parameters = pika.ConnectionParameters(hostname,
+    credentials = pika.PlainCredentials(username, password)
+    parameters = pika.ConnectionParameters(hostname,
                                        5672,
                                        virtualhost,
                                        credentials)
 
-connection = pika.BlockingConnection(parameters)
+    connection = pika.BlockingConnection(parameters)
 
-channel = connection.channel()
+    channel = connection.channel()
 
-exchange_name = 'patient_data'
-channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
+    exchange_name = 'patient_data'
+    channel.exchange_declare(exchange=exchange_name, exchange_type='topic')
 
-result = channel.queue_declare('', exclusive=True)
-queue_name = result.method.queue
+    result = channel.queue_declare('', exclusive=True)
+    queue_name = result.method.queue
 
-binding_keys = "#"
+    binding_keys = "#"
 
-if not binding_keys:
-    sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
-    sys.exit(1)
+    if not binding_keys:
+        sys.stderr.write("Usage: %s [binding_key]...\n" % sys.argv[0])
+        sys.exit(1)
 
-for binding_key in binding_keys:
-    channel.queue_bind(
-        exchange=exchange_name, queue=queue_name, routing_key=binding_key)
+    for binding_key in binding_keys:
+        channel.queue_bind(
+            exchange=exchange_name, queue=queue_name, routing_key=binding_key)
 
-print(' [*] Waiting for logs. To exit press CTRL+C')
+    print(' [*] Waiting for logs. To exit press CTRL+C')
 
 
 def callback(ch, method, properties, body):
@@ -92,7 +93,8 @@ def callback(ch, method, properties, body):
         DBtools.add_patient(f_name, l_name, mrn, zipcode, patient_status)
 
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
+if __name__ == "__main__":
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=True)
 
-channel.start_consuming()
+    channel.start_consuming()
