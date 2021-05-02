7b06b6e1d85e333616b7c9a4759ce785dd4ff3f0
Dillon <43819022+Dcode99@users.noreply.github.com>
2021-05-02 13:34:49 -0400

    Update APIs.py

--- APIs.py
+++ APIs.py
@@ -1,9 +1,9 @@
 import json
 import requests
 import pymongo
-import DBtools
-import subscriber
 import pymysql
+
+import subscriber
 
 
 def getAPI():
@@ -18,55 +18,69 @@ def mongo_connect():
     client = pymongo.MongoClient(database_url)
 
     # Connecting to the database
-    mydb = client['patients']
+    mydb = client['hospitals']
 
     # Connecting the to collection
     # TO DO: add collection name
+    collection_name = 'hospital'
     col = mydb[collection_name]
-    d = col.delete_many()
 
 
-# restart mysql database and set cursor
-dbCursor = DBtools.restartDB()
-# get API
-path = getAPI()
-
-# getteam API
-if path is "/api/getteam":
-    status = 1
-    team_info = {
-        'team_name': 'Arnoldillo',
-        'Team_member_sids': {912141777, 910823113},
-        'app_status_code': status
-    }
-    jsonString = json.dumps(team_info)
-
-# reset API, needs to reset relational DB and CEP
-elif path is "/api/reset":
-    try:
-        # Connecting the to collection
-        dbCursor = DBtools.restartDB()
-        return_info = {
-            'reset_status_code': 1
+def run_apis():
+    # restart mysql database and set cursor
+    dbCursor = DBtools.restartDB()
+    # get API
+    path = getAPI()
+    jsonString = {}
+    # getteam API
+    if path == "/api/getteam":
+        status = 1
+        team_info = {
+            'team_name': 'Arnoldillo',
+            'Team_member_sids': {912141777, 910823113},
+            'app_status_code': status
         }
-    except Exception as e:
+        jsonString = json.dumps(team_info)
+
+    # reset API, needs to reset relational DB and CEP
+    elif path == "/api/reset":
+        try:
+            # Connecting the to collection
+            dbCursor = DBtools.restartDB()
+            return_info = {
+                'reset_status_code': 1
+            }
+        except Exception as e:
+            return_info = {
+                'reset_status_code': 0
+            }
+        jsonString = json.dumps(return_info)
+    elif path.split("/")[1] == "/api/gethospital":
+        hospital_id = path.split("/")[2]
+        query = "SELECT max_capacity AS total_beds," \
+                " (max_capacity-current_capacity) AS available_beds," \
+                " zip_code FROM hospitals" \
+                " WHERE hospital_id is " + str(hospital_id)
+
+        return_info = dbCursor.execute(query)
+        jsonString = json.dumps(return_info)
+
+    elif path == "/api/testcount":
         return_info = {
-            'reset_status_code': 0
+            'positive_count': subscriber.positive_count,
+            'negative_count': subscriber.negative_count
         }
-    jsonString = json.dumps(return_info)
-
-elif path is "/api/gethospital":
-    query = "SELECT max_capacity AS total_beds," \
-            " (max_capacity-current_capacity) AS available_beds," \
-            " zip_code FROM hospitals" \
-            " WHERE hospital_id is " + str(id)
-
-    return_info = dbCursor.execute(query)
-    jsonString = json.dumps(return_info)
-
-elif path is "/api/testcount":
-    return_info = {
-        'positive_count': subscriber.positive_count,
-        'negative_count': subscriber.negative_count
-    }
-    jsonString = json.dumps(return_info)
+        jsonString = json.dumps(return_info)
+    elif path == "/api/zipalertlist":
+        # TODO
+        return_info = {}
+        jsonString = json.dumps(return_info)
+    elif path == "/api/alertlist":
+        # TODO
+        return_info = {}
+        jsonString = json.dumps(return_info)
+    elif path.split("/")[1] == "getpatient":
+        # TODO
+        return_info = {}
+        jsonString = json.dumps(return_info)
+    return jsonString
