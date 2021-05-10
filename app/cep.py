44e11399d9606b00474078949fd8e92ada89eb1a
naroarnold <nrar222@nrar222.cs.engr.uky.edu>
2021-05-10 09:15:29 -0400

    Updated cep with more robust queries

--- app/cep.py
+++ app/cep.py
@@ -1,13 +1,15 @@
 from PySiddhi.core.SiddhiManager import SiddhiManager
 from PySiddhi.core.query.output.callback.QueryCallback import QueryCallback
-from PySiddhi.core.uttil.EventPrinter import PrintEvent
+from PySiddhi.core.util.EventPrinter import PrintEvent
 
 
 siddhiManager = SiddhiManager()
 
 #Define Siddhi query
 patientStream = "define stream PatientStream (first_name string, last_name string, mrn int, zip_code int, patient_status_code int); " + \
-		"@info(name = 'status') from PatientStream#window.time(15 sec) as f join PatientStream#window.time(30 sec) as t on f.zip_code == t.zip_code select f.count() as fCount, t.count() as tCount insert into OutputStream;"
+		"@info(name = 'mid') from PatientStream#window.time(15 sec) as f join PatientStream#window.time(30 sec) as t on f.zip_code == t.zip_code select f.zip_code as zip, f.count() as fCount, t.count() as tCount insert into MidStream; " + \
+		"@info(name = 'zipalert') from MidStream[3*fCount > 2*tCount] select zip insert into OutputStream; " + \
+		"@info(name = 'statealert') from OutputStream#window.time(15 sec) select count() as EmergencyZipCount having EmergencyZipCount > 4 insert into StateStream; "
 
 #Generate runtime
 patientRuntime = siddhiManager.createSiddhiAppRuntime(patientStream)
@@ -17,8 +19,12 @@ patientRuntime = siddhiManager.createSiddhiAppRuntime(patientStream)
 class QueryCallbackImpl(QueryCallback):
 	def receive(self, timestamp, inEvents, outEvents):
 		PrintEvent(timestamp, inEvents, outEvents)
-patientRuntime.addCallback("status", QueryCallbackImpl())
 
+#Add listener for stream that alerts when a zip code experiences growth
+patientRuntime.addCallback("zipalert", QueryCallbackImpl())
+
+#Add listener for stream that alerts when five or more zip codes are in alert state
+patientRuntime.addCallback("statealert", QueryCallbackImpl())
 
 #input handler that processes events to siddhi
 inputHandler = patientRuntime.getInputHandler("PatientStream")
